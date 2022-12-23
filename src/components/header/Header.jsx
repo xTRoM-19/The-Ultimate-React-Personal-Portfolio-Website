6e5647533317c93dfd60b77ad3d017bcdbd9357f
XUAN THANH <haxuanthanh196@Gmail.com>
2022-12-23 16:31:08 +0700

    upload

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>  
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>XUAN THANH</h1>
+        <h5 className="text-light"> 
+          Fullstack Developer
+        </h5>
+        <CTA/>
+      </div>
+    </header> 
   )
 }
 
