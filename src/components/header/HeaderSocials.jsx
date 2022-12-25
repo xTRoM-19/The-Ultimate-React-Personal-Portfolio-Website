e0e20defb01e756749143051bccc9a45fdfe0d5d
XUAN THANH <haxuanthanh196@Gmail.com>
2022-12-26 00:14:12 +0700

    upload

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-// import {BsLinkedin} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href='https://linkedin.com' target="_blank">BsLinkedin</a>
+      <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>
       <a href='https://github.com' target="_blank"></a>
       <a href='https://dribble.com' target="_blank"></a>
     </div>
